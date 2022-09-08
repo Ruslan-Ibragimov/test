eab66eefcd168d6b20d4ab4d29c352c7857804ae
Ruslan Ibragimov <ibragimov.rus.rus@gmail.com>
2022-09-09 00:05:40 +0300

    commit

--- src/Application.java
+++ src/Application.java
@@ -125,7 +125,6 @@ public class Application {
         SeaBord board = new SeaBord();
         board.shoot(0, 0, "m");
         board.shoot(2, 0, "h");
-        board.shoot(6, 9, "h");
         board.shoot(5, 5, "d");
         for (int i = 0; i < board.getField().length; i++) {
             for (int j = 0; j < board.getField()[0].length; j++) {
@@ -135,5 +134,3 @@ public class Application {
         }
     }
 }
-
-
